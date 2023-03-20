a59d03e136491bb2566805a79f8cec92c85e1322
kouwa stephane <kksasteph888@gmail.com>
2023-03-20 04:56:53 +0100

    Maj

--- src/BlocksContentsTypeListBuilder.php
+++ src/BlocksContentsTypeListBuilder.php
@@ -9,24 +9,28 @@ use Drupal\Core\Entity\EntityInterface;
  * Provides a listing of Blocks contents type entities.
  */
 class BlocksContentsTypeListBuilder extends ConfigEntityListBuilder {
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function buildHeader() {
     $header['label'] = $this->t('Blocks contents type');
     $header['id'] = $this->t('Machine name');
+    $header['description'] = $this->t('Description');
     return $header + parent::buildHeader();
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
+    $row['description'] = $entity->getDescription();
     // You probably want a few more properties here...
     return $row + parent::buildRow($entity);
   }
-
+  
 }
