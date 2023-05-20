ab0c8abbc45baaa1df4fed27ac673cdf963f1d8e
kouwa stephane <kksasteph888@gmail.com>
2023-05-20 19:08:44 +0200

    Maj

--- src/BlocksContentsListBuilder.php
+++ src/BlocksContentsListBuilder.php
@@ -12,8 +12,6 @@ use Drupal\Core\Link;
  * @ingroup blockscontent
  */
 class BlocksContentsListBuilder extends EntityListBuilder {
-  protected $field_access = \Drupal\domain_access\DomainAccessManagerInterface::DOMAIN_ACCESS_FIELD;
-  protected $field_source = \Drupal\domain_source\DomainSourceElementManagerInterface::DOMAIN_SOURCE_FIELD;
   
   /**
    *
@@ -23,8 +21,6 @@ class BlocksContentsListBuilder extends EntityListBuilder {
     $header['id'] = $this->t('Blocks contents ID');
     $header['type'] = 'Type';
     $header['name'] = $this->t('Name');
-    $header[$this->field_access] = 'field_access';
-    $header[$this->field_source] = 'field_source';
     return $header + parent::buildHeader();
   }
   
@@ -39,8 +35,6 @@ class BlocksContentsListBuilder extends EntityListBuilder {
     $row['name'] = Link::createFromRoute($entity->label(), 'entity.blocks_contents.edit_form', [
       'blocks_contents' => $entity->id()
     ]);
-    $row[$this->field_access] = $entity->get($this->field_access)->target_id;
-    $row[$this->field_source] = $entity->get($this->field_source)->target_id;
     return $row + parent::buildRow($entity);
   }
   
